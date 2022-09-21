a696b26308b5ecb67870fd2b088d54b6257c4a3c
pwierdak <paulinawierdak95@gmail.com>
2022-09-21 20:18:30 +0200

    Object and Class

--- src/Flat.java
+++ src/Flat.java
@@ -1,10 +1,28 @@
 public class Flat {
-    int floor;
-    String city;
-    boolean balcony;
+    private int floor;
+    private String city;
+    private boolean balcony;
     Flat (int floor, String city, boolean balcony){
         this.balcony=balcony;
         this.floor=floor;
         this.city=city;
+    }
+    public int getFloor(){
+        return floor;
+    }
+    public String getCity(){
+        return city;
+    }
+    public boolean getBalcony(){
+        return balcony;
+    }
+    public void setBalcony(boolean balcony){
+        this.balcony=balcony;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setFloor(int floor){
+        this.floor=floor;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args){
         Flat mieszkanie1=new Flat(5, "Berlin", true);
-        System.out.println(mieszkanie1);
+        System.out.println(mieszkanie1.getFloor());
+        System.out.println(mieszkanie1.getCity());
+        System.out.println(mieszkanie1.balcony());
+        mieszkanie1.setBalcony(false);
+        System.out.println(mieszkanie1.getBalcony());
+        mieszkanie1.setCity("Amsterdam");
+        Flat mieszkanie2=new Flat(10, "Wroclaw", true);
+        mieszkanie2.setFloor(0);
+        System.out.println(mieszkanie2.getFloor());
     }
 
 }
